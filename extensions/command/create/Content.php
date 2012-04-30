8eda5e8def637b30f8de33ed50d52bd69f8d2820
Dan Applegate <dan.applegate@rokkan.com>
2012-04-30 18:28:10 -0400

    Initial commit, initial content creator template

---
+++ extensions/command/create/Content.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace lithe\extensions\command\create;
+
+class Content extends \lithium\console\command\Create\Model {
+
+}
---
+++ extensions/command/create/template/content.txt.php
@@ -0,0 +1,9 @@
+namespace {:namespace};
+
+class {:class} extends \lithium\data\Model {
+
+	public $belongsTo = array('Authors');
+
+	public $validates = array();
+
+}
